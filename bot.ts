2bf6b1820a7cdf74abdee7654e63d614c51a9bbb
dcdunkan <70066170+dcdunkan@users.noreply.github.com>
2022-02-08 01:27:26 +0530

    Support for single files and file path

--- bot.ts
+++ bot.ts
@@ -1,36 +1,70 @@
 import "https://deno.land/x/dotenv@v3.2.0/load.ts";
 import { Bot, InputFile } from "https://deno.land/x/grammy@v1.7.0/mod.ts";
-import { join } from "https://deno.land/std@0.125.0/path/mod.ts";
+import { basename, join } from "https://deno.land/std@0.125.0/path/mod.ts";
 import { prettyBytes } from "https://deno.land/std@0.125.0/fmt/bytes.ts";
 
-const bot = new Bot(Deno.env.get("BOT_TOKEN") as string, {
-  client: { apiRoot: Deno.env.get("API_ROOT") ?? "https://api.telegram.org" },
+const BOT_TOKEN = Deno.env.get("BOT_TOKEN") ?? "";
+const ADMIN_ID = parseInt(Deno.env.get("ADMIN_ID") as string);
+const API_ROOT = Deno.env.get("API_ROOT") ?? "https://api.telegram.org";
+
+const bot = new Bot(BOT_TOKEN, {
+  client: { apiRoot: API_ROOT },
 });
 
 // Making the bot only accessible to the owner.
-const ADMIN_ID = parseInt(Deno.env.get("ADMIN_ID") as string);
 bot.use(async (ctx, next) =>
   ctx.from?.id === ADMIN_ID ? await next() : undefined
 );
 
 bot.command("start", async (ctx) => {
   await ctx.reply(
-    "Hello! Syntax: /folder <path>\nExample: /folder /home/user/Pictures/",
+    "Hello! I can help you upload your local files to here." +
+      "\nSyntax: /upload <path>" +
+      "\nExamples: /upload /home/user/Pictures/" +
+      "\nOr /upload /home/user/Pictures/image.jpg" +
+      "\n\nRepository: github.com/dcdunkan/file-upload-bot",
   );
 });
 
-bot.command("folder", async (ctx, next) => {
-  if (!ctx.match) return await ctx.reply("Please provide a folder path.");
+bot.command("upload", async (ctx, next) => {
+  if (!ctx.match) return await ctx.reply("Please provide a file/folder path.");
 
   const path = ctx.match as string;
-  const { message_id } = await ctx.reply("Getting file list...");
+  const { message_id } = await ctx.reply("Reading path...");
+
+  const exists = await fileExists(path);
+  if (!exists) return await ctx.reply("File/Folder not found.");
+
+  // Single file upload.
+  if (exists.isFile) {
+    const filename = basename(path);
+    await ctx.api.editMessageText(
+      ctx.chat.id,
+      message_id,
+      `Uploading <code>${filename}</code> from <code>${path}</code>`,
+      { parse_mode: "HTML" },
+    );
+
+    await ctx.replyWithDocument(new InputFile(path, filename), {
+      caption: `Filename: <code>${filename}</code>` +
+        `\nPath: <code>${path}</code>` +
+        `\nSize: ${prettyBytes(exists.size)}` +
+        `\nCreated at: <code>${exists.birthtime?.toUTCString()}</code>`,
+      parse_mode: "HTML",
+    });
+
+    return await ctx.api.deleteMessage(ctx.chat.id, message_id);
+  }
+
   const files = await getFileList(path);
   if (files.length === 0) return await ctx.reply("No files found.");
 
   await ctx.api.editMessageText(
     ctx.chat.id,
     message_id,
-    `Uploading ${files.length} files from <code>${path}</code>`,
+    `Uploading ${files.length} file${
+      files.length > 1 ? "s" : ""
+    } from <code>${path}</code>`,
     { parse_mode: "HTML" },
   );
 
@@ -46,36 +80,46 @@ bot.command("folder", async (ctx, next) => {
     // Double check.
     if (!await fileExists(file.path)) {
       await ctx.reply(
-        `File <code>${file.name}</code> not found. Skipping.\nPath: <code>${file.path}</code>`,
+        `'<code>${file.name}</code>' not found. Skipping.\nPath: <code>${file.path}</code>`,
         { parse_mode: "HTML" },
       );
+      continue;
     }
 
     // Update progress message.
     await ctx.api.editMessageText(
       ctx.chat.id,
       message_id,
-      `Uploading ${files.length} files from <code>${path}</code>
-[${i + 1}/${files.length}] <code>${files[i].name}</code>`,
+      `Uploading [${i + 1}/${files.length}] <code>${
+        files[i].name
+      }</code> from <code>${path}</code>`,
       { parse_mode: "HTML" },
     );
 
-    await ctx.replyWithDocument(new InputFile(file.path, file.name), {
-      caption: `Filename: <code>${file.name}</code>` +
-        `\nPath: <code>${file.path}</code>` +
-        `\nSize: ${prettyBytes(file.size)}` +
-        `\nCreated at: <code>${file.created_at}</code>`,
-      parse_mode: "HTML",
-    });
-    // Pause for a bit.
-    await pause(300);
+    try {
+      await ctx.replyWithDocument(new InputFile(file.path, file.name), {
+        caption: `Filename: <code>${file.name}</code>` +
+          `\nPath: <code>${file.path}</code>` +
+          `\nSize: ${prettyBytes(file.size)}` +
+          `\nCreated at: <code>${file.created_at}</code>`,
+        parse_mode: "HTML",
+      });
+      // Pause for a bit.
+      await pause(200);
+    } catch (error) {
+      await ctx.reply(`Failed to upload <code>${file.name}</code>.`);
+      console.error(error);
+      continue;
+    }
   }
 
   await ctx.api.editMessageText(ctx.chat.id, message_id, "Uploaded!");
   await ctx.unpinChatMessage(message_id);
 
   await ctx.reply(
-    `Successfully uploaded ${files.length} files from <code>${path}</code>`,
+    `<b>Successfully uploaded ${files.length} file${
+      files.length > 1 ? "s" : ""
+    } from</b> <code>${path}</code>`,
     { parse_mode: "HTML" },
   );
 });
@@ -128,4 +172,4 @@ function pause(ms: number) {
 }
 
 bot.catch(console.error);
-bot.start({ onStart: () => console.log("Bot started") });
+bot.start({ onStart: ({ username }) => console.log(`${username} started.`) });
